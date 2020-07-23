066e46042b88751cdf9978af0ee2fb810f516178
Mustafa <cana66331@gmail.com>
2020-07-23 14:42:50 +0200

    frondend added

---
+++ build/precache-manifest.aecdb46e71c1dec1e09797cab32b3099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676f504a76da503fa81c64ba9ef4d246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2649bb01787528b8e1b3",
+    "url": "/static/css/main.4187a5f6.chunk.css"
+  },
+  {
+    "revision": "261379cd12f7a257a30b",
+    "url": "/static/js/2.72903e02.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.72903e02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2649bb01787528b8e1b3",
+    "url": "/static/js/main.0ebfc857.chunk.js"
+  },
+  {
+    "revision": "8dd7441d529e73d61d47",
+    "url": "/static/js/runtime-main.aaa24d6c.js"
+  }
+]);
